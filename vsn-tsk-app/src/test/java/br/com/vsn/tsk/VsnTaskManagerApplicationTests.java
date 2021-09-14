0b192568683fd0fce817254717340d839c824d10
Alexandre Visnardi <alexandre.visnardi@trescon.com.br>
2021-09-13 22:04:06 -0300

    Reestrutura pastas maven.

---
+++ vsn-tsk-app/src/test/java/br/com/vsn/tsk/VsnTaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vsn.tsk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VsnTaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
