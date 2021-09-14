ced971eadeb8872ce615ffa84cb88ee3d1264655
Alexandre Visnardi <alexandre.visnardi@trescon.com.br>
2021-09-13 22:04:06 -0300

    Reestrutura pastas maven.

---
+++ src/main/java/br/com/vsn/tsk/VsnTaskManagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.vsn.tsk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VsnTaskManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VsnTaskManagerApplication.class, args);
+	}
+
+}
