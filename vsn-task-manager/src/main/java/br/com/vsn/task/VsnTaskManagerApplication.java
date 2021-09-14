b2495399cac889e77ef9afceb5d8f0dc3d49a6c7
Alexandre Visnardi <alexandre.visnardi@trescon.com.br>
2021-09-13 21:01:07 -0300

    Commit inicial.

---
+++ vsn-task-manager/src/main/java/br/com/vsn/task/VsnTaskManagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.vsn.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VsnTaskManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VsnTaskManagerApplication.class, args);
+	}
+
+}
---
+++ vsn-task-manager/src/test/java/br/com/vsn/task/VsnTaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vsn.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VsnTaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
