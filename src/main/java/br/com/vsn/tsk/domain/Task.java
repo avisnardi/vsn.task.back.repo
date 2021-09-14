c266179d176026768073d9ae9a70a2646af8467d
Alexandre Visnardi <alexandre.visnardi@trescon.com.br>
2021-09-13 22:44:57 -0300

    Definir relacionamentos das classes de domínio.

--- src/main/java/br/com/vsn/tsk/domain/Task.java
+++ src/main/java/br/com/vsn/tsk/domain/Task.java
@@ -1,8 +1,29 @@
 package br.com.vsn.tsk.domain;
 
-public class Task {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Task implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
+	
+	@ManyToOne
+	@JoinColumn(name = "owner_id")
 	private Owner owner;
 	
 	public Task() {
