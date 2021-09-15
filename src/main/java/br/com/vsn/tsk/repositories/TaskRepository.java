9691a9fc17e492ecb7fa9c397c96975e0d96ff04
Alexandre Visnardi <alexandre.visnardi@trescon.com.br>
2021-09-14 22:11:50 -0300

    Criação dos repositorios iniciais.

---
+++ src/main/java/br/com/vsn/tsk/repositories/TaskRepository.java
@@ -0,0 +1,11 @@
+package br.com.vsn.tsk.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.vsn.tsk.domain.Task;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Integer> {
+
+}
